2832fcfd262b60449b4516a85730bf6976930c5b
Sagar Survase(Android Devoloper) <49334643+sagarsurvse@users.noreply.github.com>
2020-10-30 20:57:34 +0530

    Add files via upload

---
+++ java/Sagar/youtubeapp/MainActivity.java
@@ -0,0 +1,95 @@
+package Sagar.youtubeapp;
+
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText searchInput;
+    private ListView videosFound;
+
+    private Handler handler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+
+        searchInput = (EditText)findViewById(R.id.search_input);
+        videosFound = (ListView)findViewById(R.id.videos_found);
+
+        handler = new Handler();
+
+        searchInput.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if(actionId == EditorInfo.IME_ACTION_DONE){
+                    searchOnYoutube(v.getText().toString());
+                    return false;
+                }
+                return true;
+            }
+        });
+
+
+    }
+
+    private List<VideoItem> searchResults;
+
+    private void searchOnYoutube(final String keywords){
+        new Thread(){
+            public void run(){
+
+//                YoutubeConnector youtubeConnector = new YoutubeConnector();
+//                youtubeConnector.setInitReq(MainActivity.this, keywords);
+
+                YoutubeConnector yc = new YoutubeConnector(MainActivity.this);
+                searchResults = yc.search(keywords);
+                handler.post(new Runnable(){
+                    public void run(){
+                        updateVideosFound();
+                    }
+                });
+            }
+        }.start();
+    }
+
+    private void updateVideosFound(){
+        ArrayAdapter<VideoItem> adapter = new ArrayAdapter<VideoItem>(getApplicationContext(), R.layout.video_item, searchResults){
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                if(convertView == null){
+                    convertView = getLayoutInflater().inflate(R.layout.video_item, parent, false);
+                }
+                ImageView thumbnail = (ImageView)convertView.findViewById(R.id.video_thumbnail);
+                TextView title = (TextView)convertView.findViewById(R.id.video_title);
+                TextView description = (TextView)convertView.findViewById(R.id.video_description);
+
+                VideoItem searchResult = searchResults.get(position);
+
+                Picasso.with(getApplicationContext()).load(searchResult.getThumbnailURL()).into(thumbnail);
+                title.setText(searchResult.getTitle());
+                description.setText(searchResult.getDescription());
+                return convertView;
+            }
+        };
+
+        videosFound.setAdapter(adapter);
+    }
+
+
+}
---
+++ java/Sagar/youtubeapp/YoutubeConnector.java
@@ -0,0 +1,162 @@
+package Sagar.youtubeapp;
+
+import android.content.Context;
+import android.text.TextUtils;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.api.client.http.HttpRequest;
+import com.google.api.client.http.HttpRequestInitializer;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.services.youtube.YouTube;
+import com.google.api.services.youtube.model.SearchListResponse;
+import com.google.api.services.youtube.model.SearchResult;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Retrofit;
+
+public class YoutubeConnector {
+    private YouTube youtube;
+    private YouTube.Search.List query;
+    private Context context;
+
+    // Your developer key goes here
+    public static final String KEY
+            = "AIzaSyBT5Hwsaj-0Df1muXIijTrSq_rrRKyDw4w";
+
+    public YoutubeConnector() {
+
+    }
+
+    public YoutubeConnector(Context context) {
+        this.context = context;
+        youtube = new YouTube.Builder(new NetHttpTransport(),
+                new JacksonFactory(), new HttpRequestInitializer() {
+            @Override
+            public void initialize(HttpRequest hr) throws IOException {
+            }
+        }).setApplicationName(context.getString(R.string.app_name)).build();
+
+        try {
+            query = youtube.search().list("id,snippet");
+            query.setKey(KEY);
+            query.setType("video");
+            query.setFields("*");
+        } catch (IOException e) {
+            Log.d("YC", "Could not initialize: " + e);
+        }
+    }
+
+    public List<VideoItem> search(String keywords) {
+        query.setQ(keywords);
+        try {
+            SearchListResponse response = query.execute();
+            List<SearchResult> results = response.getItems();
+
+            List<VideoItem> items = new ArrayList<VideoItem>();
+            for (SearchResult result : results) {
+                VideoItem item = new VideoItem();
+                item.setTitle(result.getSnippet().getTitle());
+                item.setDescription(result.getSnippet().getDescription());
+                item.setThumbnailURL(result.getSnippet().getThumbnails().getDefault().getUrl());
+                item.setId(result.getId().getVideoId());
+                items.add(item);
+            }
+
+            String ids = "";
+            for (VideoItem videoItem : items) {
+                if (TextUtils.isEmpty(ids)) {
+                    ids = videoItem.getId();
+                } else {
+                    ids = ids + "," + videoItem.getId();
+                }
+            }
+            retrieveDataByRetrofit(context, ids);
+
+            return items;
+        } catch (IOException e) {
+            Log.d("YC", "Could not search: " + e);
+            return null;
+        }
+    }
+
+    public void setInitReq(Context context, String searchText) {
+        RequestQueue queue = Volley.newRequestQueue(context);
+
+        String url = "https://www.googleapis.com/youtube/v3/search?part=id&snippet&q=" + searchText + "&type=video&key=" + KEY;
+
+// Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Log.v("MIMO_SAHA::", "Res: " + response);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.v("MIMO_SAHA::", "Err: " + error.getMessage());
+            }
+        });
+        queue.add(stringRequest);
+    }
+
+    private void retrieveAllData(Context context, String ids) {
+
+        RequestQueue queue = Volley.newRequestQueue(context);
+
+        String url = "https://www.googleapis.com/youtube/v3/videos?id=" + ids + "&key=" + KEY
+                + "&part=snippet,contentDetails,statistics,status";
+
+// Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Log.v("MIMO_SAHA::", "Res: " + response);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.v("MIMO_SAHA::", "Err: " + error.getMessage());
+            }
+        });
+        queue.add(stringRequest);
+    }
+
+    private void retrieveDataByRetrofit(Context context, String ids) {
+        Retrofit retrofit = NetworkClient.getRetrofitClient();
+        FileApis fileApis = retrofit.create(FileApis.class);
+
+        Call<PlaylistItemListResponse> call = fileApis.getVideoInfo(ids, KEY, "snippet,contentDetails,statistics,status");
+
+        call.enqueue(new Callback<PlaylistItemListResponse>() {
+            @Override
+            public void onResponse(Call<PlaylistItemListResponse> call,
+                                   retrofit2.Response<PlaylistItemListResponse> response) {
+
+                if (response.body() != null) {
+                    PlaylistItemListResponse playlistItemListResponse = response.body();
+                }
+
+                Log.v("MIMO_SAHA::", "Response: " + call + " >> " + response);
+            }
+
+            @Override
+            public void onFailure(Call<PlaylistItemListResponse> call, Throwable t) {
+                Log.v("MIMO_SAHA::", "Response: " + call + " >> " + t);
+            }
+        });
+    }
+}
